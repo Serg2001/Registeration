00734251cff9192fb84bb7eb4563fd9b49d6a5e1
User <User@DSHH-20>
2025-04-07 12:00:36 +0400

    f

--- DTOs/UserDTO.cs
+++ DTOs/UserDTO.cs
@@ -9,8 +9,8 @@ namespace Registeration.DTOs
 
         [Required, DataType(DataType.Password)]
         [StringLength(100, MinimumLength = 8, ErrorMessage = "Գաղտնաբառը պետք է պարունակի ամենաքիչը 8նիշ")]
-        [RegularExpression(@"^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$",
-        ErrorMessage = "Գաղտնաբառը պետք է պարունակի ամենաքիչը մեկ մեծատառ, մեկ թվանշան, և մեկ սիմվոլ.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$",
+        ErrorMessage = "Գաղտնաբառը պետք է պարունակի ամենաքիչը մեկ մեծատառ,մեկ փոէրատառ, մեկ թվանշան, և մեկ սիմվոլ.")]
         public string Password { get; set; } = string.Empty;
 
         [Required, Compare(nameof(Password)), DataType(DataType.Password)]
